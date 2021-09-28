9fb891d87c1d2323b121c7830397d6ea4e633efb
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-29 00:17:24 +0530

    Update and rename YoneRobot/modules/wiki.py to Lucas/modules/wiki.py

--- Lucas/modules/wiki.py
+++ Lucas/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from YoneRobot import dispatcher
-from YoneRobot.modules.disable import DisableAbleCommandHandler
+from Lucas import dispatcher
+from Lucas.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
