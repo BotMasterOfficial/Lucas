37dfbeee2b9db06082544397de835e1b20f3974e
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-28 20:24:40 +0530

    Update sample_config.py

--- Lucas/sample_config.py
+++ Lucas/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/YoneRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Lucas/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
