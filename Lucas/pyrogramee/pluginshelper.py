d79623a497faa046cd509c88fb92d5094a4b583d
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-28 20:58:55 +0530

    Update pluginshelper.py

--- Lucas/pyrogramee/pluginshelper.py
+++ Lucas/pyrogramee/pluginshelper.py
@@ -12,7 +12,7 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from Lucas  import OWNER_ID, SUPPORT_CHAT
+from Lucas import OWNER_ID, SUPPORT_CHAT
 from Lucas import pbot
 
 
