b03ae2a60b81fee5cfb977bbcc3a5f7556bab763
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-29 00:20:19 +0530

    Update and rename YoneRobot/modules/ud.py to Lucas/modules/ud.py

--- Lucas/modules/ud.py
+++ Lucas/modules/ud.py
@@ -1,6 +1,6 @@
 import requests
-from YoneRobot import dispatcher
-from YoneRobot.modules.disable import DisableAbleCommandHandler
+from Lucas import dispatcher
+from Lucas.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
