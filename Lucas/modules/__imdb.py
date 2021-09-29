a9e424e23794b46c2f42e0762d9dc0231bbf7163
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-29 12:08:25 +0530

    Update and rename YoneRobot/modules/__imdb.py to Lucas/modules/__imdb.py

--- Lucas/modules/__imdb.py
+++ Lucas/modules/__imdb.py
@@ -1,11 +1,11 @@
-from YoneRobot import telethn as tbot
+from Lucas import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from YoneRobot.events import register
+from Lucas.events import register
 
 langi = "en"
 
