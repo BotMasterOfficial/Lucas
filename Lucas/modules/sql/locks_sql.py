862902776102f3f002e220916d346b8c8e3f9f63
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-30 16:20:47 +0530

    Update and rename YoneRobot/modules/sql/locks_sql.py to Lucas/modules/sql/locks_sql.py

--- Lucas/modules/sql/locks_sql.py
+++ Lucas/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from YoneRobot.modules.sql import SESSION, BASE
+from Lucas.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
