bb9eb7248c2ee7e0bc14980a5aeed722ed347950
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-30 16:23:39 +0530

    Update and rename YoneRobot/modules/sql/notes_sql.py to Lucas/modules/sql/notes_sql.py

--- Lucas/modules/sql/notes_sql.py
+++ Lucas/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from YoneRobot.modules.helper_funcs.msg_types import Types
-from YoneRobot.modules.sql import BASE, SESSION
+from Lucas.modules.helper_funcs.msg_types import Types
+from Lucas.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
