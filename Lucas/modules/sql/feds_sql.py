e9597fbb475837ae2d3921388578ccac5e1190a1
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-30 15:16:07 +0530

    Update and rename YoneRobot/modules/sql/feds_sql.py to Lucas/modules/sql/feds_sql.py

--- Lucas/modules/sql/feds_sql.py
+++ Lucas/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from YoneRobot import dispatcher
-from YoneRobot.modules.sql import BASE, SESSION
+from Lucas import dispatcher
+from Lucas.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
