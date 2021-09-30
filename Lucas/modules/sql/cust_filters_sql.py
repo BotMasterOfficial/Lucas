af18389c21ce8e34538882d0f1212433db768b70
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-09-30 15:10:31 +0530

    Update and rename YoneRobot/modules/sql/cust_filters_sql.py to Lucas/modules/sql/cust_filters_sql.py

--- Lucas/modules/sql/cust_filters_sql.py
+++ Lucas/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from YoneRobot.modules.helper_funcs.msg_types import Types
-from YoneRobot.modules.sql import BASE, SESSION
+from Lucas.modules.helper_funcs.msg_types import Types
+from Lucas.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
