9c4a576fcb9173d84774679801a01d3b0c16801b
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-01 20:04:51 +0530

    Update and rename YoneRobot/modules/eval.py to Lucas/modules/eval.py

--- Lucas/modules/eval.py
+++ Lucas/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from YoneRobot import LOGGER, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import dev_plus
+from Lucas import LOGGER, dispatcher
+from Lucas.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
@@ -76,7 +76,7 @@ def do(func, bot, update):
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "YoneRobot/modules/helper_funcs/temp.txt"), "w"
+        os.path.join(os.getcwd(), "Lucas/modules/helper_funcs/temp.txt"), "w"
     ) as temp:
         temp.write(body)
 
