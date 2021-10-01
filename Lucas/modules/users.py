4f9a8e09d8b2637fc9a66c844bf36bb02b02ac13
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-01 08:14:46 +0530

    Update and rename YoneRobot/modules/users.py to Lucas/modules/users.py

--- Lucas/modules/users.py
+++ Lucas/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import YoneRobot.modules.sql.users_sql as sql
-from YoneRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from YoneRobot.modules.sql.users_sql import get_all_users
+import Lucas.modules.sql.users_sql as sql
+from Lucas import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from Lucas.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from Lucas.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
