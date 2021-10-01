d5efdfe31ce23f645b1560dec2ae01464c35f622
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-01 19:45:46 +0530

    Update and rename YoneRobot/modules/ping.py to Lucas/modules/ping.py

--- Lucas/modules/ping.py
+++ Lucas/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from YoneRobot import StartTime, dispatcher
-from YoneRobot.modules.helper_funcs.chat_status import sudo_plus
-from YoneRobot.modules.disable import DisableAbleCommandHandler
+from Lucas import StartTime, dispatcher
+from Lucas.modules.helper_funcs.chat_status import sudo_plus
+from Lucas.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
