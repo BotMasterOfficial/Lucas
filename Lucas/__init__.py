005dc3c83289bdad8ca420808be4c26f703f5986
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-11 12:31:54 +0530

    Update __init__.py

--- Lucas/__init__.py
+++ Lucas/__init__.py
@@ -84,6 +84,7 @@ if ENV:
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     AI_API_KEY = os.environ.get("AI_API_KEY", None)
@@ -157,6 +158,7 @@ else:
     WORKERS = Config.WORKERS
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
+    STRICT_GMUTE = Config.STRICT_GMUTE
     CASH_API_KEY = Config.CASH_API_KEY
     TIME_API_KEY = Config.TIME_API_KEY
     AI_API_KEY = Config.AI_API_KEY
