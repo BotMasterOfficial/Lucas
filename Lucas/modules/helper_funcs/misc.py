9c5274e9272f4366491bcd916fb5caa0deda8ead
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-11 20:29:44 +0530

    Update misc.py

--- Lucas/modules/helper_funcs/misc.py
+++ Lucas/modules/helper_funcs/misc.py
@@ -76,9 +76,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     return pairs
 
 
-def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False
-) -> None:
+def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
