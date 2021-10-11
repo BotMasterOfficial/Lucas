2bfcbfe40a0a32fffe4b514fbde03eb9282cd74f
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-11 20:15:07 +0530

    Update chat_status.py

--- Lucas/modules/helper_funcs/chat_status.py
+++ Lucas/modules/helper_funcs/chat_status.py
@@ -34,30 +34,15 @@ def is_sudo_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
 
 
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
-    if (
-        chat.type == "private"
-        or user_id in DRAGONS
-        or user_id in DEV_USERS
-        or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
-    ):  # Count telegram and Group Anonymous as admin
+    if chat.type == "private"
+            or user_id in DRAGONS
+            or user_id in DEV_USERS
+            or chat.all_members_are_administrators
         return True
+    
     if not member:
-        with THREAD_LOCK:
-            # try to fetch from cache first.
-            try:
-                return user_id in ADMIN_CACHE[chat.id]
-            except KeyError:
-                # keyerror happend means cache is deleted,
-                # so query bot api again and return user status
-                # while saving it in cache for future useage...
-                chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
-                admin_list = [x.user.id for x in chat_admins]
-                ADMIN_CACHE[chat.id] = admin_list
-
-                return user_id in admin_list
-    else:
-        return member.status in ("administrator", "creator")
+        member = chat.get_member(user_id)
+    return member.status in ('administrator', 'creator')
 
 
 def is_bot_admin(chat: Chat, bot_id: int, bot_member: ChatMember = None) -> bool:
